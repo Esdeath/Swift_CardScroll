56583366364968d20a4c01062e8d3d6272fdcc50
ayaseeri@126.com <mymisaka@gmail.com>
2016-02-28 21:19:21 +0800

    添加pod库

---
+++ Swift_CardScroll/MRConst.swift
@@ -0,0 +1,16 @@
+//
+//  MRConst.swift
+//  Swift_CardScroll
+//
+//  Created by Asuna on 16/2/21.
+//  Copyright © 2016年 Asuna. All rights reserved.
+//
+
+import UIKit
+
+class MRConst: NSObject {
+
+    
+    static let width = 324.0/750.0 * UIScreen.mainScreen().bounds.width
+    
+}
